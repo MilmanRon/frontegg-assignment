1be3a76a211a73bbef99dbce765ee18a0ea86528
MilmanRon <milmanron@gmail.com>
2025-07-28 15:24:37 +0300

    feat: configure Frontegg authentication in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,31 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { FronteggAppModule } from '@frontegg/angular';
+import { FronteggAppOptions } from '@frontegg/types';
 
 import { routes } from './app.routes';
+
+const fronteggConfig: FronteggAppOptions = {
+  contextOptions: {
+    baseUrl: 'https://app-tkyztdfvr4u7.frontegg.com',
+    clientId: '064e6075-72a4-4430-bc70-7fc5e9ab31fd',
+  },
+  authOptions: {
+    // keepSessionAlive: true // Uncomment this in order to maintain the session alive
+  },
+  hostedLoginBox: true,
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    importProvidersFrom(FronteggAppModule.forRoot(fronteggConfig)),
+  ],
 };
