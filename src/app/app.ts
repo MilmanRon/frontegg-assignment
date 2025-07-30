3976e46cf4b358f3421fdbd3f93bc28a6fdc21ad
MilmanRon <milmanron@gmail.com>
2025-07-30 11:39:16 +0300

    feat: implement tenant switching with dropdown selector

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,28 +1,34 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
 import {
-  FronteggAuthService,
-  FronteggAppService,
-  ContextHolder,
-} from '@frontegg/angular';
+  ChangeDetectorRef,
+  Component,
+  computed,
+  effect,
+  inject,
+  OnDestroy,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { FronteggAuthService, FronteggAppService } from '@frontegg/angular';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App implements OnInit, OnDestroy {
   protected title = 'frontegg-assignment';
 
-  private fronteggAuthService = inject(FronteggAuthService);
+  fronteggAuthService = inject(FronteggAuthService);
   private fronteggAppService = inject(FronteggAppService);
-  private router = inject(Router);
 
   isLoading = true;
   loadingSubscription: Subscription;
   user?: any;
+
+  tenantsState = signal<any>(null);
 
   constructor() {
     this.loadingSubscription = this.fronteggAppService.isLoading$.subscribe(
@@ -33,6 +39,10 @@ export class App implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.fronteggAuthService?.user$.subscribe((user) => {
       this.user = user;
+    });
+
+    this.fronteggAuthService.tenantsState$.subscribe((state) => {
+      this.tenantsState.set(state);
     });
   }
 
@@ -47,6 +57,15 @@ export class App implements OnInit, OnDestroy {
 
   showAdminPortal(): void {
     this.fronteggAppService?.showAdminPortal();
+  }
+
+  onTenantChange(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    const tenantId = target.value;
+
+    this.fronteggAuthService.switchTenant({
+      tenantId: tenantId,
+    });
   }
 
   ngOnDestroy(): void {
